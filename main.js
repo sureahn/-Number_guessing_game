e53b88c962da908d6958a978285d187a3a7d8b68
Jungmin Ahn <jungminahn829@gmail.com>
2024-02-13 03:11:53 +1100

    02_commit

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-//Set random number
-//User input number, and click go
-//if user correct the number, print Correct!
-//If random number is lower than User number, print Down!
-//If random number is higher than User number, print Up!
-
-//If user click the Reset btn, game reset.
-//If user try 5 times, game done.(btn disabled), if user correct the number btn disabled
-//We alert that if user type number out range of 1-100. We don't count that chance.
-//We alert that id user type same number as before. We don't count that chance.
-
 let computerNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
@@ -19,7 +8,7 @@ let gameOver = false;
 let chanceArea = document.getElementById("chance-area");
 let history=[];
 
-playButton.addEventListener("click",play); //(event,변수(괄호넣지않기))
+playButton.addEventListener("click",play);
 resetButton.addEventListener("click",reset);
 userInput.addEventListener("focus",function () {
     userInput.value = "";
@@ -27,18 +16,12 @@ userInput.addEventListener("focus",function () {
 
 function pickRandomNum() {
         computerNum = Math.floor(Math.random() * 100) + 1; 
-        // Math.random() : pick random number between 0 < Num < 1
-        // * 100 : random print decimal number so * 100 > make the random number as two digit left
-        // Math.floor() : discard(truncate) the decimal part(소수점 아래 버린다)
-        // + 1 : Math.random () print lower than 1, not 1. 
-        //      So originally, function has a value between 0-99.
-        //      But we need a value between 1-100, so add 1.
         console.log("Answer",computerNum);
     
 }
 
 function play() {
-        let userValue = userInput.value; //bring value of user-input
+        let userValue = userInput.value; 
 
         //validate check
         if (userValue < 1 || userValue > 100) {
